b2ab9162d77fb104b0224105f2108cd78a3af639
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-04-25 20:00:41 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+string SenhaCorreta, Senhadigitada;
+Console.Write("Digite sua senha:");
+Senhadigitada = Console.ReadLine();
+Console.Clear();
+Console.WriteLine("digite novamente");
+SenhaCorreta = Console.ReadLine()!;
+if(SenhaCorreta == Senhadigitada)
+{//senha certa
+Console.ForegroundColor = ConsoleColor.Green;
+Console.Write("usuario logado com sucesso");}
+
+else
+{//senha errada
+Console.ForegroundColor = ConsoleColor.Red;
+Console.Write("Senha errada");}
+
+Console.ResetColor();
